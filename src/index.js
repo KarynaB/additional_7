92150d6082d20c374c3f8acc0c25e7e83c9f2fab
Karyna <36641408+KarynaB@users.noreply.github.com>
2018-03-04 21:25:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  for (var i=0; i<9; i++){
+  for (var j=0; j<9; j++){
+  if(matrix[i][j]===0)
+  {
+  matrix[i][j]=[];
+  }
+}}
+  return matrix;
 }
